e29eb0e947a5b61fefd57bf3a6975d53e8e5da2a
Henry Emily <henryandcharlie123@gmail.com>
2026-01-15 00:07:12 +0700

    new changes

--- app.js
+++ app.js
@@ -37,11 +37,17 @@ const client = new Client({
     ]
 });
 
+client.newUsers = {};
+
 client.on(Events.ClientReady, (readyClient) => console.log(`[SERVER] ${readyClient.user.username} it's ready!`));
-client.on(Events.GuildMemberAdd, (member) => {
+client.on(Events.GuildMemberAdd, async(member) => {
     if(member.user.bot) return;
     let channel = member.guild.channels.cache.get("708518759287095299");
     if(!channel || !channel.isTextBased()) return;
+    
+    if(client.newUsers[member.guild.id] && client.newUsers[member.guild.id].includes(member.user.id)) return;
+    if(client.newUsers[member.guild.id]) client.newUsers[member.guild.id].push(member.user.id);
+    else client.newUsers[member.guild.id] = [member.user.id];
 
     member.roles.add("767609668091510785").catch(console.log);
     member.roles.add("718747564756828193").catch(console.log);
@@ -56,11 +62,14 @@ client.on(Events.GuildMemberAdd, (member) => {
         .setTitle(`WELCOME TO THE ${member.guild.name}!`)
         .setThumbnail(member.user.displayAvatarURL({ size: 1024 }))
         .setDescription("Since you are new here, please read ⁠<#719038315357012050>, ⁠<#719038487214686278>, ⁠and <#741891605920743435>. After read all of them, verify yourself and **ENJOY YOUR STAY!**");
-    channel
-        .send({
-            content: `Welcome to the **${member.guild.name}**, <@${member.user.id}>!`,
-            embeds: [embed]
-        })
-        .catch(console.log);
+    try {
+        await channel
+            .send({
+                content: `Welcome to the **${member.guild.name}**, <@${member.user.id}>!`,
+                embeds: [embed]
+            });
+    } catch (error) {
+        console.log(error);
+    }
 });
 client.login(process.env.TOKEN);
